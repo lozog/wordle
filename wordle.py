e0ebae0ac061d5c6ae8abd40a8228baa5720fd31
Liam Ozog <liam@ada.support>
2022-01-23 13:26:04 -0500

    move guess analysis to analyze_guess()

--- wordle.py
+++ wordle.py
@@ -1,10 +1,29 @@
 from enum import Enum
+from pprint import pprint
 
 
 class LetterResult(Enum):
     INCORRECT = 0
     INCORRECT_POSITION = 1
     CORRECT_POSITION = 2
+
+
+def analyze_guess(guess, word):
+    unguessed_letters = list(word)
+    guess_result = []
+    for letter_pair in zip(guess, word):
+        res = LetterResult.INCORRECT
+        if letter_pair[0] == letter_pair[1]:
+            unguessed_letters.remove(letter_pair[0])
+            res = LetterResult.CORRECT_POSITION
+        elif letter_pair[0] in unguessed_letters:
+            unguessed_letters.remove(letter_pair[0])
+            res = LetterResult.INCORRECT_POSITION
+
+        print(f"{letter_pair[0]} {letter_pair[1]}: {res}")
+        guess_result.append(res)
+
+    return guess_result
 
 
 WORD_LIST = ["crimp"]
@@ -15,16 +34,8 @@ guess_count = 0
 word = WORD_LIST[0]
 print(f"Enter guess #{guess_count+1}: ", end="")
 guess = input()
+# TODO: ensure guess is 5 letters long
 
-unguessed_letters = list(word)
-for letter_pair in zip(guess, word):
-    # 0 -> incorrect, 1 -> incorrect position, 2 -> correct position
-    res = LetterResult.INCORRECT
-    if letter_pair[0] == letter_pair[1]:
-        unguessed_letters.remove(letter_pair[0])
-        res = LetterResult.CORRECT_POSITION
-    elif letter_pair[0] in unguessed_letters:
-        unguessed_letters.remove(letter_pair[0])
-        res = LetterResult.INCORRECT_POSITION
-
-    print(f"{letter_pair[0]} {letter_pair[1]}: {res}")
+res = analyze_guess(guess, word)
+if set(res) == set([LetterResult.CORRECT_POSITION]):
+    print("you win!")
