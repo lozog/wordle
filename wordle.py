60f6026a36455847789d204aac5a26d529a1e6f5
Liam Ozog <liam@ada.support>
2022-01-23 19:05:35 -0500

    add letter_results dict to keep track of guessed letters. also add debug printing

--- wordle.py
+++ wordle.py
@@ -7,11 +7,23 @@ class LetterResult(Enum):
     INCORRECT = 0
     INCORRECT_POSITION = 1
     CORRECT_POSITION = 2
+    UNUSED = 3
 
 
 class GameResult(Enum):
     LOSS = 0
     WIN = 1
+
+
+DEBUG_MODE = False
+def print_debug(string):
+    if DEBUG_MODE:
+        print(string)
+
+
+def pprint_debug(string):
+    if DEBUG_MODE:
+        pprint(string)
 
 
 def get_input(guess_count, word_length):
@@ -27,20 +39,22 @@ def get_input(guess_count, word_length):
     return guess.lower()
 
 
-def analyze_guess(guess, word):
+def analyze_guess(guess, word, letter_results):
     unguessed_letters = list(word)
     guess_result = []
     for letter_pair in zip(guess, word):
-        res = LetterResult.INCORRECT
-        if letter_pair[0] == letter_pair[1]:
-            unguessed_letters.remove(letter_pair[0])
-            res = LetterResult.CORRECT_POSITION
-        elif letter_pair[0] in unguessed_letters:
-            unguessed_letters.remove(letter_pair[0])
-            res = LetterResult.INCORRECT_POSITION
+        letter = letter_pair[0]
+        letter_result = LetterResult.INCORRECT
+        if letter == letter_pair[1]:
+            unguessed_letters.remove(letter)
+            letter_result = LetterResult.CORRECT_POSITION
+        elif letter in unguessed_letters:
+            unguessed_letters.remove(letter)
+            letter_result = LetterResult.INCORRECT_POSITION
 
-        print(f"{letter_pair[0]}: {res}")
-        guess_result.append(res)
+        guess_result.append(letter_result)
+        letter_results[letter] = letter_result
+        print(f"{letter}: {letter_result}")
 
     return guess_result
 
@@ -49,15 +63,19 @@ WORD_LENGTH = 5
 MAX_GUESS_COUNT = 6
 input_file = open(f"word_list_{WORD_LENGTH}", 'r')
 WORD_LIST = input_file.read().splitlines()
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 while True:
     word_choice = random.randint(0, len(WORD_LIST) - 1)
     word = WORD_LIST[word_choice].lower()
+    print_debug(word)
     game_result = GameResult.LOSS
+    letter_results = {letter:LetterResult.UNUSED for letter in ALPHABET}
 
     for guess_count in range(MAX_GUESS_COUNT):
         guess = get_input(guess_count, WORD_LENGTH)
-        res = analyze_guess(guess, word)
+        res = analyze_guess(guess, word, letter_results)
+        pprint_debug(letter_results)
         if set(res) == set([LetterResult.CORRECT_POSITION]):
             game_result = GameResult.WIN
             break
